4f5941ef81c1ef05decd3251520a087b9ad54c95
Stefan Milovanovic <stefan@192.168.1.6>
2024-01-15 12:44:11 +0100

    feat: mappings

--- keyboards/sofle/keymaps/smilovanovic/keymap.c
+++ keyboards/sofle/keymaps/smilovanovic/keymap.c
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, /* --- */ KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO, /* --- */ KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RCTL(OSM(KC_RALT)),
         // ----------------------------------------------------------------------------------- //
-        KC_NO, KC_LALT, KC_LCMD, KC_SPC, LT(_LOWER, KC_ESC), /* --- */ LT(_RAISE, KC_ENT), KC_BSPC, RSFT_T(KC_RCMD), KC_NO, KC_NO
+        KC_NO, KC_LALT, KC_LCMD, KC_SPC, LT(_LOWER, KC_ESC), /* --- */ LT(_RAISE, KC_ENT), KC_BSPC, RSFT_T(OSM(KC_RCMD)), KC_NO, KC_NO
     ),
     [_LOWER] = LAYOUT(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, /* --- */ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_ESC), KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, /* --- */ KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
         MO(_NUMPAD), KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_NO, /* --- */ KC_NO, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_PSLS,
         // ----------------------------------------------------------------------------------- //
-        KC_NO, KC_LALT, KC_LCMD, KC_SPC, MO(_ADJUST), /* --- */ KC_BSPC, KC_RSFT, KC_RCTL, KC_NO, KC_NO
+        KC_NO, KC_LALT, KC_LCMD, KC_SPC, MO(_ADJUST), /* --- */ MO(_ADJUST), KC_BSPC, KC_RSFT, KC_RCTL, KC_NO
     ),
     [_RAISE] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* --- */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LSFT_T(KC_ESC), KC_NO, KC_NO, KC_CUT, KC_LALT, KC_LCMD, /* --- */ KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
         KC_LCTL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* --- */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
-        KC_NO, KC_LALT, KC_LCMD, KC_SPC, MO(_ADJUST), /* --- */ KC_BSPC, KC_RSFT, KC_RCTL, KC_NO, KC_NO
+        KC_NO, KC_LALT, KC_LCMD, KC_SPC, MO(_ADJUST), /* --- */ MO(_ADJUST), KC_BSPC, KC_RSFT, KC_RCTL, KC_NO
     ),
     [_ADJUST] = LAYOUT(
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* --- */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
